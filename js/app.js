a2bb8dad646c55e656ce63de662b7dd253fed967
Sergio Souza <emailsouza@gmail.com>
2018-07-05 17:15:40 -0600

    east and west traffic flowing

--- js/app.js
+++ js/app.js
@@ -12,8 +12,10 @@ let chickenHeight = 40;
 let normalFlowSpeed = 4;
 let istTrafficFlowNormal = true; 
 
-let vehicles = [];  // array to hold vehicles
-let onRoad = []; // array of vehicles that are on the road
+const vehicles = [];  // array to hold vehicles
+// const westVehicles = [];
+let westBoundRoad = []; // array of vehicles that go on the westBound road
+let eastBoundRoad = []; // array of vehicles that go on the westBound road
 
 // Objects
 class Vehicles {
@@ -26,7 +28,7 @@ class Vehicles {
     this.weight = weight;
     this.speedX = speedX;
     this.directionX = 1; // positive goes from left to right
-    this.velocityX = this.speedX * this.directionX;
+    // this.velocityX = this.speedX * this.directionX;
     this.color = color;
     vehicles.push(this);
   }
@@ -56,18 +58,22 @@ const bottomRoad = new Road((canvas.height / 2) + (canvas.height / 6));
 
 
 // initial traffic (vehicles on road at the start of the game)
-// onRoad.push(vehicles.shift(0, 1));
+// westBoundRoad.push(vehicles.shift(0, 1));
 
 // ########################################################################################################################
 // ######################################### CREATE AND MANAGE SHAPES / OBJECTS ######################################################################
 // ########################################################################################################################
 
 // Draw Vehicles
-const drawVehicle = (vehicle) => {
+const drawVehicleWestBound = (vehicle) => {
   vehicle.x -= vehicle.speedX;  // adjust x by speedX on each call, gives object a velocity 
   drawRect(vehicle.x, vehicle.y, vehicle.width, vehicle.height, vehicle.color); 
 }
-
+const drawVehicleEastBound = (vehicle) => {
+  vehicle.x += vehicle.speedX;  // adjust x by speedX on each call, gives object a velocity 
+  drawRect(vehicle.x, vehicle.y, vehicle.width, vehicle.height, vehicle.color);
+}
+ 
 // Draw Lanes (proportinal to the canvas size) - takes a road object and the Y coordinate as parameters
 const drawRoad = (road, topY) => {
   drawRect(0, topY, canvas.width, road.roadWidth, 'gray'); // road tarmac
@@ -91,51 +97,57 @@ const drawEverything = () => {
   drawRoad(topRoad, topRoad.topYToCenterDraw); // top road
   drawRoad(bottomRoad, bottomRoad.topYToCenterDraw); // bottom road
   drawRect(chickenX, chickenY, chickenWidth, chickenHeight, 'yellow'); // draw chicken
-  if (onRoad.length > 0) {
-    onRoad.forEach(element => {  // vehicles of the onRoad array
-      drawVehicle(element);
+  if (westBoundRoad.length > 0) {
+    westBoundRoad.forEach(element => {  // vehicles of the westBoundRoad array
+      drawVehicleWestBound(element);
+    });
+  }
+  if (eastBoundRoad.length > 0) {
+    eastBoundRoad.forEach(element => {  // vehicles of the westBoundRoad array
+      // element.speedX = -element.speedX
+      drawVehicleEastBound(element);
     });
   }
 }
 
 // Feed Vehicles onto the Road
-// Remove object from the vehicles[] array and add it to onRoad[] array
-const getOnEastBoundRoad = () => {
+// Remove object from the vehicles[] array and add it to westBoundRoad[] array
+const getOnWestBoundRoad = () => {
   if (vehicles.length > 0) { // if there are any vehicles available to go on the road
     const laneNumber = Math.floor(Math.random() * 3) + 1; // num between 1 and 3
-    console.log('laneNumber is ' + laneNumber);
     switch (laneNumber) {
       case 1:
         vehicles[0].x = canvas.width;
-        vehicles[0].velocityX *= -1;  // reverses velocity 
+        // vehicles[0].velocityX *= -1;  // reverses velocity 
         vehicles[0].y = topRoad.topY - topRoad.laneWidth * 1.25;
         break;
       case 2:
         vehicles[0].x = canvas.width;
-        vehicles[0].velocityX *= -1;  // reverses velocity 
+        // vehicles[0].velocityX *= -1;  // reverses velocity 
         vehicles[0].y = topRoad.topY - topRoad.laneWidth * 0.25;
         break;
       case 3:
         vehicles[0].x = canvas.width;
-        vehicles[0].velocityX *= -1;  // reverses velocity 
+        // vehicles[0].velocityX *= -1;  // reverses velocity 
         vehicles[0].y = topRoad.topY + topRoad.laneWidth * 0.7
         break;
       default:
         break;
     }
     
-    onRoad.push(vehicles.shift(0, 1));
+    westBoundRoad.push(vehicles.shift(0, 1));
   }
 }
 
-const getOnWestBoundRoad = () => {
+const getOnEastBoundRoad = () => {
   if (vehicles.length > 0) { // if there are any vehicles available to go on the road
     const laneNumber = Math.floor(Math.random() * 3) + 1; // num between 1 and 3
-    console.log('laneNumber is ' + laneNumber);
     switch (laneNumber) {
       case 1:
         vehicles[0].x = 0;
         // vehicles[0].vehicle = -vehicles[0].velocityX
+        console.log(vehicles[0].x);
+        // vehicles[0].velocityX *= -1;  // reverses velocity 
         vehicles[0].y = topRoad.topY + topRoad.laneWidth * 2.7
         break;
       case 2:
@@ -149,12 +161,9 @@ const getOnWestBoundRoad = () => {
       default:
         break;
     }
-
-    onRoad.push(vehicles.shift(0, 1));
+    eastBoundRoad.push(vehicles.shift(0, 1));
   }
 }
-
-
 
 // ########################################################################################################################
 // ######################################### CHECK / ADJUST MOVEMENTS ######################################################################
@@ -166,7 +175,7 @@ const isThereCollision = () => {
   const cX2 = chickenX + chickenWidth;
   const cY1 = chickenY;
   const cY2 = chickenY + chickenHeight
-  onRoad.forEach(vehicle => {
+  westBoundRoad.forEach(vehicle => {
     vX1 = vehicle.x;  
     vX2 = vehicle.x + vehicle.width;
     vY1 = vehicle.y;
@@ -183,17 +192,17 @@ const isThereCollision = () => {
 
 // Adjust Vehicle Speed
 const slowVehicleSpeedEastBound = () => {  
-  for (let i = 0; i < onRoad.length - 1; i += 1) {
-    // const vX1 = onRoad[i].x;
-    // const vX2 = onRoad[] 
-    const vehicleX1 = onRoad[i].x;
-    const vehicleX2 = vehicleX1 + onRoad[i].width;
-    const vehicleSpeed = onRoad[i].speedX;
-    const nextVehicleX1 = onRoad[i + 1].x;
+  for (let i = 0; i < westBoundRoad.length - 1; i += 1) {
+    // const vX1 = westBoundRoad[i].x;
+    // const vX2 = westBoundRoad[] 
+    const vehicleX1 = westBoundRoad[i].x;
+    const vehicleX2 = vehicleX1 + westBoundRoad[i].width;
+    const vehicleSpeed = westBoundRoad[i].speedX;
+    const nextVehicleX1 = westBoundRoad[i + 1].x;
 
     if (nextVehicleX1 - vehicleX2 <= 10) { //
       // console.log('WATCH OUT !!!!');
-      onRoad[i + 1].speedX = vehicleSpeed;
+      westBoundRoad[i + 1].speedX = vehicleSpeed;
     }
   }
 }
@@ -201,7 +210,7 @@ const slowVehicleSpeedEastBound = () => {
 // Resume Vehicle Speed
 resumeSpeedEastBound = () => {
   if (!istTrafficFlowNormal) {
-    onRoad.forEach(vehicle => {
+    westBoundRoad.forEach(vehicle => {
       // console.log(checkForCollision());
       // console.log(checkForCollision());
       console.log('Resuming speed');     
@@ -242,13 +251,13 @@ window.onload = () => {
   animate();
   const framesPerSecond = 2;
   setInterval(() => {
+    getOnWestBoundRoad();
     getOnEastBoundRoad(); // insertion of new cars on the road
-    // getOnWestBoundRoad();
   }, 1000 / framesPerSecond);
 
   setInterval(() => {
     resumeSpeedEastBound(); // resume traffic speed
-  }, 3000);
+  }, 2000);
 
   canvas.addEventListener('mousemove', (event) => {
     const mousePosition = getMousePosition(event);
